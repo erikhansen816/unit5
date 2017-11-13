0134537c5331ea8572a7fd0cb5693956bac248e4
erikhansen816 <31485788+erikhansen816@users.noreply.github.com>
2017-11-13 14:47:00 -0500

    Updated from Brython Server: 11/13/2017 2:46:59 PM

--- middleWord.py
+++ middleWord.py
@@ -1,1 +1,11 @@
+#Erik Hansen
+#11/13/17
+#middleWord.py - prints out middle word
 
+words = input('Enter words: ').split(' ')
+middle = len(words)
+
+if middle%2 == 0:
+    print(words[middle/2-1], words[middle/2])
+else:
+    print(words[middle//2])
