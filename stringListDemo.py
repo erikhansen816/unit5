e5893967ef192ae25f7b6b7eb16fe7dcf252502c
erikhansen816 <31485788+erikhansen816@users.noreply.github.com>
2017-11-16 14:10:54 -0500

    Updated from Brython Server: 11/16/2017 2:10:54 PM

--- stringListDemo.py
+++ stringListDemo.py
@@ -1,1 +1,13 @@
+#Erik Hansen
+#11/16/2017
+#stringListDemo.py - print out words that start with a vowel
+
+words = input('Enter a list of words: ').split(' ')
+
+for word in words:
+    if word[0] in 'aeiouAEIOU':
+        print(word)
+    
+
+
 
