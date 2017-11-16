3a8465cee333b8e914999a3008353d88c93dd912
erikhansen816 <31485788+erikhansen816@users.noreply.github.com>
2017-11-16 14:13:48 -0500

    Updated from Brython Server: 11/16/2017 2:13:48 PM

--- warmup13.py
+++ warmup13.py
@@ -1,1 +1,11 @@
-
+#Erik Hansen
+#11/16/17
+#warmup13.py
+from random import randint
+list1 = []
+for i in range(1,21):
+    list1.append(randint(1,100))
+print(list1)
+print(min(list1))
+print(max(list1))
+print(sum(list1))
