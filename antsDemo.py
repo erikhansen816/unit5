02ea8765fcc481384a0ff94b5efcda48bee92c03
erikhansen816 <31485788+erikhansen816@users.noreply.github.com>
2017-11-20 14:27:14 -0500

    Update antsDemo.py

--- antsDemo.py
+++ antsDemo.py
@@ -1,1 +1,19 @@
+#Erik Hansen
+#11/20/17
+#antsDemo.py - how to use lists with graphics
+
+from ggame import *
+from random import randint
+
+WIDTH = 800
+HEIGHT = 400
+
+if __name__ == '__main__':
+    red = Color(0xFF0000,1)
+    ant = CircleAsset(10,LineStyle(1,red),red)
+    
+    for i in range(10):
+        Sprite(ant,randint(1,WIDTH),randint(1,HEIGHT))
+    
+    App().run()
 
