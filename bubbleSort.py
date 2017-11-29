725ac7f2d23f08dd0b50f448b99ee12b0d189b6f
erikhansen816 <31485788+erikhansen816@users.noreply.github.com>
2017-11-29 13:57:14 -0500

    Updated from Brython Server: 11/29/2017 1:57:14 PM

--- bubbleSort.py
+++ bubbleSort.py
@@ -1,1 +1,58 @@
+#Erik Hansen
+#11/29/17
+#insertionSort.py
+
+from random import randint
+from time import time
+
+N = 100 #how many numbers will be sorted
+
+
+"""procedure bubbleSort( A : list of sortable items )
+    n = length(A)
+    repeat 
+        swapped = false
+        for i = 1 to n-1 inclusive do
+            /* if this pair is out of order */
+            if A[i-1] > A[i] then
+                /* swap them and remember something changed */
+                swap( A[i-1], A[i] )
+                swapped = true
+            end if
+        end for
+    until not swapped
+end procedure"""
+
+
+def mySort(A):
+    n = len(A)
+    swapped = False
+    while swapped == False:
+        for i in range(1,n):
+            if A[i-1] > A[i]:
+                A[i-1], A[i] = A[i], A[i-1]
+                swapped = True
+    print(A)
+    return A
+
+if __name__ == '__main__':
+    
+    #make a list of N random numbers between 1 and N
+    numbers = [0]*N
+    for i in range(N):
+        numbers[i] = randint(1,N)
+        
+    pythonSort = sorted(numbers) #Python's sort
+    
+    #time how long your sort takes
+    t1 = time()
+    numbers = mySort(numbers)
+    t2 = time()
+       
+    #print whether the sort worked or not
+    try:
+        assert(numbers == pythonSort)
+        print('Your sort took', t2-t1, 'seconds')
+    except:
+        print('Your sort did not work')
 
