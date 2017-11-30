11a509bf16b9d81565254969638b41db3a8b3caf
erikhansen816 <31485788+erikhansen816@users.noreply.github.com>
2017-11-30 14:53:39 -0500

    Updated from Brython Server: 11/30/2017 2:53:39 PM

--- combSort.py
+++ combSort.py
@@ -1,1 +1,68 @@
+#Erik Hansen
+#11/30/17
+#insertionSort.py
+
+from random import randint
+from time import time
+
+N = 100 #how many numbers will be sorted
+
+
+"""function combsort(array input)
+
+    gap := input.size // Initialize gap size
+    shrink := 1.3 // Set the gap shrink factor
+    sorted := false
+
+    loop while sorted = false
+        // Update the gap value for a next comb
+        gap := floor(gap / shrink)
+        if gap > 1
+            sorted := false // We are never sorted as long as gap > 1
+        else
+            gap := 1
+            sorted := true // If there are no swaps this pass, we are done
+        end if
+
+        // A single "comb" over the input list
+        i := 0
+        loop while i + gap < input.size // See Shell sort for a similar idea
+            if input[i] > input[i+gap]
+                swap(input[i], input[i+gap])
+                sorted := false
+                // If this assignment never happens within the loop,
+                // then there have been no swaps and the list is sorted.
+             end if
+
+             i := i + 1
+         end loop
+         
+     end loop
+ end function"""
+
+
+def mySort(A):
+    
+
+if __name__ == '__main__':
+    
+    #make a list of N random numbers between 1 and N
+    numbers = [0]*N
+    for i in range(N):
+        numbers[i] = randint(1,N)
+        
+    pythonSort = sorted(numbers) #Python's sort
+    
+    #time how long your sort takes
+    t1 = time()
+    numbers = mySort(numbers)
+    t2 = time()
+       
+    #print whether the sort worked or not
+    try:
+        assert(numbers == pythonSort)
+        print('Your sort took', t2-t1, 'seconds')
+    except:
+        print('Your sort did not work')
+
 
